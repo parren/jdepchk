4bee8132b306dad8134ba6f47253f6633a1e7000
Peter Arrenbrecht <peter.arrenbrecht@gmail.com>
2011-07-08 16:11:48 +0200

    classes: moved asm source to internal package

--- src/main/java/ch/parren/jdepchk/classes/asm/Edge.java
+++ src/main/java/ch/parren/jdepchk/classes/asm/Edge.java
@@ -27,44 +27,49 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.objectweb.asm;
+package ch.parren.jdepchk.classes.asm;
 
 /**
- * Information about an exception handler block.
+ * An edge in the control flow graph of a method body. See {@link Label Label}.
  * 
  * @author Eric Bruneton
  */
-class Handler {
+class Edge {
 
     /**
-     * Beginning of the exception handler's scope (inclusive).
+     * Denotes a normal control flow graph edge.
      */
-    Label start;
+    static final int NORMAL = 0;
 
     /**
-     * End of the exception handler's scope (exclusive).
+     * Denotes a control flow graph edge corresponding to an exception handler.
+     * More precisely any {@link Edge} whose {@link #info} is strictly positive
+     * corresponds to an exception handler. The actual value of {@link #info} is
+     * the index, in the {@link ClassWriter} type table, of the exception that
+     * is catched.
      */
-    Label end;
+    static final int EXCEPTION = 0x7FFFFFFF;
 
     /**
-     * Beginning of the exception handler's code.
+     * Information about this control flow graph edge. If
+     * {@link ClassWriter#COMPUTE_MAXS} is used this field is the (relative)
+     * stack size in the basic block from which this edge originates. This size
+     * is equal to the stack size at the "jump" instruction to which this edge
+     * corresponds, relatively to the stack size at the beginning of the
+     * originating basic block. If {@link ClassWriter#COMPUTE_FRAMES} is used,
+     * this field is the kind of this control flow graph edge (i.e. NORMAL or
+     * EXCEPTION).
      */
-    Label handler;
+    int info;
 
     /**
-     * Internal name of the type of exceptions handled by this handler, or
-     * <tt>null</tt> to catch any exceptions.
+     * The successor block of the basic block from which this edge originates.
      */
-    String desc;
+    Label successor;
 
     /**
-     * Constant pool index of the internal name of the type of exceptions
-     * handled by this handler, or 0 to catch any exceptions.
+     * The next edge in the list of successors of the originating basic block.
+     * See {@link Label#successors successors}.
      */
-    int type;
-
-    /**
-     * Next exception handler block info.
-     */
-    Handler next;
+    Edge next;
 }
